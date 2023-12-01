3beb9f25effe909b07d47e2c7afaf07397141bc0
sangam0207 <127228515+sangam0207@users.noreply.github.com>
2023-12-01 13:06:08 +0530

    Update conn.js

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-mongoose.connect('mongodb://localhost:27017/portfolio')
+mongoose.connect(process.env.CONNECTION)
 .then(()=>{
     console.log('connection is successful')
 })
